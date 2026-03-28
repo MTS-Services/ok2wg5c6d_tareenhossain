73355eaf60e2dfa5a23b90486dae54a8a3af26a2
dev-sumon <akhtaruzzamansumon7@gmail.com>
2026-03-28 16:55:01 +0600

    some update

---
+++ app/Models/Category.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\CategoryStatus;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'image',
+        'status',
+
+        // Audit columns
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'created_by',
+        'updated_by',
+        'deleted_by',
+        'restored_by',
+        'restored_at',
+    ];
+    
+    protected $casts = [
+        'status' => CategoryStatus::class,
+    ];
+}
---
+++ database/migrations/2026_03_28_104241_create_categories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Enums\CategoryStatus;
+use App\Traits\AuditColumnsTrait;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    use AuditColumnsTrait;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->string('status')->index(CategoryStatus::ACTIVE->value);
+
+            
+            $table->timestamps();
+            $this->addAdminAuditColumns($table);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
